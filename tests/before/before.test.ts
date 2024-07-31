b9862e301515f231d18fc75b103a561e16c62490
foxmon <jjockrod@naver.com>
2024-07-31 18:11:42 +0900

    before

--- tests/before/before.test.ts
+++ tests/before/before.test.ts
@@ -7,4 +7,10 @@ describe("before()", () => {
 
     expect(result).toBe(true);
   });
+
+  it("returns false if the first date after the given second one", () => {
+    const result = before(new Date(2024, 1, 1), new Date(2023, 1, 1));
+
+    expect(result).toBe(false);
+  });
 });
